9d2a3ebecb838e9978a9d90cf512b9adf13511ac
Coach Ken <kenppoomm0102@gmail.com>
2026-06-26 22:12:12 +0800

    清除剩餘部署衝突標記

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-/**
-* This configuration file lets you run `$ sanity [command]` in this folder
-* Go to https://www.sanity.io/docs/cli to learn more.
-**/
-import { defineCliConfig } from 'sanity/cli'
-
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
-
-export default defineCliConfig({ api: { projectId, dataset } })
-=======
 'use client'
 
 /**
@@ -36,4 +24,3 @@ export default defineConfig({
     visionTool({defaultApiVersion: apiVersion}),
   ],
 })
->>>>>>> 93c6b6d (更新網站 SEO 與內容)
--- sanity.config.ts
+++ sanity.config.ts
@@ -8,26 +8,12 @@ import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 
-<<<<<<< HEAD
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-=======
->>>>>>> 93c6b6d (更新網站 SEO 與內容)
 import {apiVersion, dataset, projectId} from './sanity/env'
 import {schema} from './sanity/schemaTypes'
 import {structure} from './sanity/structure'
 
 export default defineConfig({
   basePath: '/studio',
-<<<<<<< HEAD
-  projectId,
-  dataset,
-  // Add and edit the content schema in the './sanity/schemaTypes' folder
-  schema,
-  plugins: [
-    structureTool({structure}),
-    // Vision is for querying with GROQ from inside the Studio
-    // https://www.sanity.io/docs/the-vision-plugin
-=======
   
   projectId,
   dataset,
@@ -35,7 +21,6 @@ export default defineConfig({
   schema,
   plugins: [
     structureTool({structure}),
->>>>>>> 93c6b6d (更新網站 SEO 與內容)
     visionTool({defaultApiVersion: apiVersion}),
   ],
 })
